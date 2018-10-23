c3f3824028d1338b772f0c3b57319b8edae5f56c
Kestutis1 <djkestutis@yahoo.com>
2018-10-23 13:11:09 +0300

    antras

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
-<?php include_once "header.php" ?>
+<?php include_once "header.php"; ?>
 
 
         <div class="container   ">
 
-            <h1> Isijunk Inspect->console </h1>
+            <h1> Labas Pasauli </h1>
 
 
 
@@ -15,19 +15,4 @@
 
 
 
-
-
-
-
-
-
-                <!--  =================================== -->
-         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-        <!-- !!! mano js failas pats zemiausias; visada failo -->
-        <script type="text/javascript" src="js/main.js"></script>
-
-    </body>
-</html>
+<?php include_once "foter.php"; ?>
