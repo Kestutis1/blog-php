1369996c9802d9edb2eda731643cbc9f261bc84a
Kestutis1 <djkestutis@yahoo.com>
2018-10-24 18:17:47 +0300

    trečiadienis

--- index.php
+++ index.php
@@ -1,18 +1,26 @@
 <?php include_once "header.php"; ?>
 
+<section>
 
-        <div class="container   ">
+  <div class="container">
+    <div class="row">
+      <article class="col-sm text-center aukstis-500">
+        <h1> Labas Pasauli </h1>
+      </article>
+      <aside class="col-sm text-center">
+        <h4> Prisijungimas </h4>
 
-            <h1> Labas Pasauli </h1>
+        <form class="" action="#" method="post">
+            <input type="email" name="email" value="" min="7" maxlength="70" title="įveskite elektroninio pašto adresą" required>
+            <input type="password" name="password" value="" min="7" maxlength="70" title="įveskite slaptažodį mažiausiai 7 simbolius" required>
+            <input type="submit" name="prisijungti" value="prisijungti">
+        </form>
 
+      </aside><!--  end rov-->
+    </div><!--  end rov-->
+  </div><!--  end Container-->
 
-
-
-
-
-
-        </div> <!--  end Container-->
-
+</section>
 
 
 <?php include_once "foter.php"; ?>
